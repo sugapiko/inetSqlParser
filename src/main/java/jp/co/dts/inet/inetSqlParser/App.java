d9ef6e449f7e60e572eb6b7695c10c0003e88446
btsugatk <btsugatk@BTSUGATK03.finet.nttdata.co.jp>
2016-02-05 16:47:50 +0900

    新規作成

---
+++ src/main/java/jp/co/dts/inet/inetSqlParser/App.java
@@ -0,0 +1,13 @@
+package jp.co.dts.inet.inetSqlParser;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
